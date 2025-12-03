4d0c5b0f3db346bcb08b3dd013c4db7fa640be4e
server1-a11y <permanajodi523@gmail.com>
2025-12-03 14:51:33 +0700

    Update server.js

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const fs = require('fs');
 const piBot = require('./run.js'); // Asumsi file ini ada dan diekspor
 
 // --- KONFIGURASI PENTING ---
-const TELEGRAM_TOKEN = '8312312035:AAHQF1p_IqgTQMfA1B-k_ls9NpOTGPzKysE';
-const WEBHOOK_URL = 'https://bot1.pileakers.net';
-const PORT = process.env.PORT || 3000;
+const TELEGRAM_TOKEN = '8465042362:AAFPtc7kgCVVYi1M_TqSqUOZzI3w3aVMLD8';
+const WEBHOOK_URL = 'https://fee.panelbot.cloud';
+const PORT = process.env.PORT || 3001;
 // ----------------------------
 
 const CONFIG_FILE = './config.json';
