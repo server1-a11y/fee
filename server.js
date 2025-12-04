954b1bb49bd4c1bdd6db2a48486b24d75c52d2f2
server1-a11y <permanajodi523@gmail.com>
2025-12-04 10:55:17 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const piBot = require('./run.js'); // Asumsi file ini ada dan diekspor
 
 // --- KONFIGURASI PENTING ---
 const TELEGRAM_TOKEN = '8465042362:AAFPtc7kgCVVYi1M_TqSqUOZzI3w3aVMLD8';
-const WEBHOOK_URL = 'https://fee.zendshost.id';
-const PORT = process.env.PORT || 1001;
+const WEBHOOK_URL = 'https://fee.panelbot.cloud';
+const PORT = process.env.PORT || 1000;
 // ----------------------------
 
 const CONFIG_FILE = './config.json';
